1dcf41b7c022cdde300b107662e86ef031942f8f
Damir Kolobaric <dkolobaric@gmail.com>
2022-04-03 18:10:44 +0200

    ProfileGitHub.js Error Fix
    ProfileGitHub.js Error Fix

--- client/src/components/profile/ProfileGithub.js
+++ client/src/components/profile/ProfileGithub.js
@@ -8,6 +8,7 @@ const ProfileGithub = ({ username, getGithubRepos, repos }) => {
   useEffect(() => {
     getGithubRepos(username);
   }, [getGithubRepos, username]);
+
   return (
     <Fragment>
       <div className="profile-github">
@@ -17,8 +18,8 @@ const ProfileGithub = ({ username, getGithubRepos, repos }) => {
         {repos === null ? (
           <Spinner />
         ) : (
-          repos.map((repo) => (
-            <div key={repo._id} className="repo bg-white p-1 my-1">
+          repos.map((repo, index) => (
+            <div key={index} className="repo bg-white p-1 my-1">
               <div>
                 <h4>
                   <a
